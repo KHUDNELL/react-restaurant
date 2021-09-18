0fe797f8f43b9fc4e79d2b06cb7bdfa08543c995
Ken Hudnell <khudnell252@gmail.com>
2021-09-18 09:46:30 -0400

    useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 
 function App() {
   // first item is state variable, second function allows us to updated state.
   const [emotion, setEmotion] = useState("happy");
-  console.log(emotion);
+  const [secondary, setSecondary] = useState("tired");
+  useEffect(() => {
+
+   console.log(`It's ${emotion} around here!`);}, [emotion]);
+
+   useEffect(() => {
+
+    console.log(`It's ${secondary} around here!`);}, [secondary]);
   return (
     <>
-     <h1>Current emotion is {emotion}.</h1>
+     <h1>Current emotion is {emotion} and {secondary}.</h1>
      <button onClick={()=>setEmotion('happy')}>Make Happy</button>
      <button onClick={()=>setEmotion('frustrated')}>Frustrate</button>
      <button onClick={()=>setEmotion('enthusiastic')}>Enthusiastic</button>
+     <button onClick={()=>setSecondary('crabby')}>crabby</button>
     </>
   )
  
